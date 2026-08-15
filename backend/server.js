289ff054df2d357c8bd633eec84fdecf7be260ad
fahadmusa725 <fahadmusa725@gmail.com>
2026-08-15 16:24:20 +0500

    Fix CORS policy and preflight headers in backend server.js

--- backend/server.js
+++ backend/server.js
@@ -52,8 +52,12 @@ app.use((req, res, next) => {
 });
 
 app.use(express.json());
-app.use(cors());
-app.use(helmet());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+app.use(helmet({ crossOriginResourcePolicy: false }));
 
 app.get('/', (req, res) => {
   res.json({ message: 'POS SaaS API is running...' });
